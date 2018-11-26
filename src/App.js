f1e89b64a96ee3b205cc3438ce0928d8916cbfae
Todd Nichols <nivram80@gmail.com>
2018-11-26 09:56:46 -0600

    move search input and list into their own components

--- src/App.js
+++ src/App.js
@@ -38,35 +38,65 @@ class App extends Component {
 
   onDismiss(id) {
     const updatedList = this.state.list.filter(item => item.objectID !== id);
-    this.setState({ list: updatedList });
+    this.setState({list: updatedList});
   }
 
   onSearchChange(event) {
-    this.setState({ searchTerm: event.target.value })
+    this.setState({searchTerm: event.target.value})
   }
 
   render() {
-    const { searchTerm, list } = this.state;
+    const {searchTerm, list} = this.state;
     return (
       <div className="App">
-        <form>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={this.onSearchChange}
-          />
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item =>
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        >
+          Search
+        </Search>
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange, children } = this.props;
+    return (
+      <form>
+        {children}
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    )
+  }
+}
+
+class Table extends Component {
+  render() {
+    const {list, pattern, onDismiss} = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
           <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
               <button
-                onClick={() => this.onDismiss(item.objectID)}
+                onClick={() => onDismiss(item.objectID)}
                 type="button"
               >
                 Dismiss
@@ -75,7 +105,7 @@ class App extends Component {
           </div>
         )}
       </div>
-    );
+    )
   }
 }
 
