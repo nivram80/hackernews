d22812f13a1663aad45bad2b53d33d2664f46f26
Todd Nichols <nivram80@gmail.com>
2018-11-26 13:35:30 -0600

    call hackernews api instead of using static list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, {Component} from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
 const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -28,12 +14,18 @@ class App extends Component {
     super(props);
 
     this.state = {
-      list,
-      searchTerm: ''
+      result: null,
+      searchTerm: DEFAULT_QUERY
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    console.log(result);
+   this.setState({ result });
   }
 
   onDismiss(id) {
@@ -45,8 +37,19 @@ class App extends Component {
     this.setState({searchTerm: event.target.value})
   }
 
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
   render() {
-    const { searchTerm, list } = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null }
+
     return (
       <div className="page">
         <div className="interactions">
@@ -58,7 +61,7 @@ class App extends Component {
           </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
@@ -77,7 +80,7 @@ const Search = ({ value, onChange, children }) =>
     />
   </form>;
 
-const Table = ({list, pattern, onDismiss}) =>
+const Table = ({ list, pattern, onDismiss }) =>
   <div className="table">
     {list.filter(isSearched(pattern)).map(item =>
       <div key={item.objectID} className="table-row">
@@ -99,10 +102,10 @@ const Table = ({list, pattern, onDismiss}) =>
     )}
   </div>;
 
-const Button = ({onClick, className = '', children }) =>
+const Button = ({ onClick, className = '', children }) =>
   <button
     onClick={onClick}
-    class={className}
+    className={className}
     type="button"
   >
     {children}
