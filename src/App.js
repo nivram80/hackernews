c87058eeffa92329c48d56e9fcd99f00052c0c03
Todd Nichols <nivram80@gmail.com>
2018-11-26 09:00:10 -0600

    add search text box

--- src/App.js
+++ src/App.js
@@ -20,27 +20,43 @@ const list = [
   },
 ];
 
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      list
+      list,
+      searchTerm: ''
     };
 
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
-  onDismiss(id){
+  onDismiss(id) {
     const updatedList = this.state.list.filter(item => item.objectID !== id);
     this.setState({ list: updatedList });
   }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        {this.state.list.map(item =>
+        <form>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
